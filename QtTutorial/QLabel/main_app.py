e85d49867cbe8740c8a9d2380f938beefab8ea67
bksj-ds <benjamin.ksj.ds@gmail.com>
2025-04-19 15:06:42 +0900

    initial commit

---
+++ QtTutorial/QLabel/main_app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 19 14:06:12 2025
+
+@author: Fata
+"""
+# main_app.py
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QObject, pyqtSignal, QTimer
+from ui_main import Ui_MainWindow
+
+# Signal 정의 클래스
+class DataEmitter(QObject):
+    updated = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.counter = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_label_text)
+        self.timer.start(1000)
+
+    def update_label_text(self):
+        self.counter += 1
+        self.updated.emit(f"Signal 업데이트: {self.counter}")
+
+# 메인 윈도우 클래스
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.emitter = DataEmitter()
+        self.emitter.updated.connect(self.label.setText)
+
+        self.pushButton.clicked.connect(self.handle_click)
+
+    def handle_click(self):
+        self.label.setText("버튼이 눌렸습니다!")
+
+app = QApplication(sys.argv)
+window = MyApp()
+window.show()
+sys.exit(app.exec_())
---
+++ README.md
@@ -0,0 +1,16 @@
+# PyQt5 GUI 튜토리얼 프로젝트
+
+🎨 **Python과 PyQt5를 활용한 데스크탑 애플리케이션 개발 실습 프로젝트입니다.**  
+초보자도 쉽게 따라할 수 있는 기본 위젯부터, 시그널/슬롯, 멀티스레드, 실시간 데이터 시각화까지 모두 담겨 있습니다.
+
+---
+
+## 📦 설치 및 실행 방법
+
+```bash
+# 1. 가상환경 생성 (Anaconda 권장)
+conda create -n pyqt5_env python=3.11
+conda activate pyqt5_env
+
+# 2. 필수 패키지 설치
+pip install PyQt5 pyqt5-tools
